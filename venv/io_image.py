2843c0ad333f15aa343631ef60360df048160952
YasinPat03 <yasinpatel04@icloud.com>
2025-02-01 12:55:30 -0500

    Base

---
+++ venv/io_image.py
@@ -0,0 +1,12 @@
+import os
+
+import cv2
+
+#read image
+image_path = os.path.join('.', 'data', 'bird.jpg')
+
+img = cv2.imread(image_path)
+
+#write image
+
+cv2.imwrite(os.path.join('.', 'data', 'bird_out.jpg'), img)
