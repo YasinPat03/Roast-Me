f7f0ea9bf53ff04957b40381775c55066b7f52f5
AariAlma <aariyan@gmail.com>
2025-02-02 04:32:32 -0500

    deleted obsolete to clean (I am the housekeeper)

--- obsolete/testai.py
+++
@@ -1,6 +0,0 @@
-import openai
-
-client = openai.OpenAI(api_key="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")
-
-response = client.models.list()
-print(response)
