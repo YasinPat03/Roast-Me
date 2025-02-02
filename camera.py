09d5a1b12bb873bab548e7761f34e9482e2d5498
AariAlma <aariyan@gmail.com>
2025-02-01 20:52:28 -0500

    Added 64 point tracking
    New data file as shape_predictor...
    camera.py works as a point detection right now, will incorporate into face_detection next

--- camera.py
+++ camera.py
@@ -1,23 +1,29 @@
 import cv2
 import dlib
 
-# Initialize webcam and face detector
 cap = cv2.VideoCapture(0)
-detector = dlib.get_frontal_face_detector()
+
+hog_face_detector = dlib.get_frontal_face_detector()
+
+dlib_facelandmark = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-
+    _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = detector(gray)
 
+    faces = hog_face_detector(gray)
     for face in faces:
-        x, y, w, h = face.left(), face.top(), face.width(), face.height()
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    cv2.imshow("Self-Roasting App", frame)
+        face_landmarks = dlib_facelandmark(gray, face)
+
+        for n in range(0, 64):
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            cv2.circle(frame, (x, y), 1, (0, 255, 255), 1)
+
+
+    cv2.imshow("Face Landmarks", frame)
+
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
